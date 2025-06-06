ae1694ded8e8ab53ec6823858c552a1e6e44281b
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2025-06-06 10:49:32 -0400

    Funding an interchain call is successful

--- contracts/src/Counter.sol
+++ contracts/src/Counter.sol
@@ -7,6 +7,7 @@ import {CallLib} from "lib/hyperlane-monorepo/solidity/contracts/middleware/libs
 import {InterchainAccountRouter} from "lib/hyperlane-monorepo/solidity/contracts/middleware/InterchainAccountRouter.sol";
 import {TypeCasts} from "lib/hyperlane-monorepo/solidity/contracts/libs/TypeCasts.sol";
 import {IInterchainGasPaymaster} from "lib/hyperlane-monorepo/solidity/contracts/interfaces/IInterchainGasPaymaster.sol";
+import "forge-std/console.sol";
 
 contract Counter {
     uint256 public number;
@@ -20,18 +21,25 @@ contract Counter {
     }
 
     function icaTest() public payable {
-
         //  InterchainAccountRouter(address(0x68B1D87F95878fE05B998F19b66F4baba5De1aed)).enrollRemoteRouterAndIsm(1380012617, TypeCasts.addressToBytes32(address(0x68B1D87F95878fE05B998F19b66F4baba5De1aed)),  TypeCasts.addressToBytes32(address(0x9A676e781A523b5d0C0e43731313A708CB607508)));
 
-        CallLib.Call memory call = CallLib.Call({to: bytes32(bytes20(0xa0Ee7A142d267C1f36714E4a8F75612F20a79720)), data: "hello", value: 0});
+        CallLib.Call memory call =
+            CallLib.Call({to: bytes32(bytes20(0xa0Ee7A142d267C1f36714E4a8F75612F20a79720)), data: "hello", value: 0});
         CallLib.Call[] memory calls = new CallLib.Call[](1);
         calls[0] = call;
+
+        
         uint32 rariDomain = 1380012617;
         address routerAddress = 0x8A93d247134d91e0de6f96547cB0204e5BE8e5D8;
         IInterchainGasPaymaster gasPaymaster = IInterchainGasPaymaster(0x457cCf29090fe5A24c19c1bc95F492168C0EaFdb);
-        uint256 gasPayment = gasPaymaster.quoteGasPayment(rariDomain, 1000);
-        IInterchainAccountRouter(routerAddress).callRemote{value: 200000}(rariDomain, calls);
+        uint256 gasPayment = gasPaymaster.quoteGasPayment(rariDomain, 50_000);
+        uint256 gasPayment2 = InterchainAccountRouter(routerAddress).quoteGasPayment(rariDomain);
+        // console.log(">>>> gasPayment (wei):", gasPayment);
+        emit Event(gasPayment);
+        IInterchainAccountRouter(routerAddress).callRemote{value: gasPayment2}(rariDomain, calls);
     }
+
+    event Event(uint256);
 
     /**
      * @notice Registers a remote InterchainAccountRouter and ISM on a given destination domain.
@@ -40,34 +48,20 @@ contract Counter {
      * @param _remoteRouter The address of the remote InterchainAccountRouter.
      * @param _remoteIsm The address of the remote InterchainSecurityModule (ISM).
      */
-    function registerIcaRouter(
-        address _icaRouter,
-        uint32 _destinationDomain,
-        address _remoteRouter,
-        address _remoteIsm
-    ) private {
+    function registerIcaRouter(address _icaRouter, uint32 _destinationDomain, address _remoteRouter, address _remoteIsm)
+        private
+    {
         // Convert addresses to bytes32 for the Enroll API.
         bytes32 routerKey = TypeCasts.addressToBytes32(_remoteRouter);
         bytes32 ismKey = TypeCasts.addressToBytes32(_remoteIsm);
 
         // Call the Hyperlane InterchainAccountRouter to enroll the router and ISM.
-        InterchainAccountRouter(_icaRouter).enrollRemoteRouterAndIsm(
-            _destinationDomain,
-            routerKey,
-            ismKey
-        );
+        InterchainAccountRouter(_icaRouter).enrollRemoteRouterAndIsm(_destinationDomain, routerKey, ismKey);
     }
 }
 
-
 interface IInterchainAccountRouter {
-    function callRemote(
-        uint32 _destinationDomain,
-        CallLib.Call[] calldata calls
-    ) external payable returns (bytes32);
+    function callRemote(uint32 _destinationDomain, CallLib.Call[] calldata calls) external payable returns (bytes32);
 
-    function getRemoteInterchainAccount(uint32 _destination, address _owner)
-        external
-        view
-        returns (address);
+    function getRemoteInterchainAccount(uint32 _destination, address _owner) external view returns (address);
 }
--- contracts/test/Counter.t.sol
+++ contracts/test/Counter.t.sol
@@ -7,6 +7,7 @@ import {Counter} from "../src/Counter.sol";
 import {CallLib} from "lib/hyperlane-monorepo/solidity/contracts/middleware/libs/Call.sol";
 import {InterchainAccountRouter} from "lib/hyperlane-monorepo/solidity/contracts/middleware/InterchainAccountRouter.sol";
 import {TypeCasts} from "lib/hyperlane-monorepo/solidity/contracts/libs/TypeCasts.sol";
+import {IInterchainGasPaymaster} from "lib/hyperlane-monorepo/solidity/contracts/interfaces/IInterchainGasPaymaster.sol";
 
 contract CounterTest is Test {
     Counter public counter;
@@ -18,9 +19,26 @@ contract CounterTest is Test {
 
     function test_ICA() public {
         vm.startPrank(address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266));
-         InterchainAccountRouter(address(0x8A93d247134d91e0de6f96547cB0204e5BE8e5D8)).enrollRemoteRouterAndIsm(1380012617, TypeCasts.addressToBytes32(address(0x68B1D87F95878fE05B998F19b66F4baba5De1aed)),  TypeCasts.addressToBytes32(address(0x9A676e781A523b5d0C0e43731313A708CB607508)));
-        counter.icaTest{ value: 250000 }();
+        InterchainAccountRouter(address(0x8A93d247134d91e0de6f96547cB0204e5BE8e5D8)).enrollRemoteRouterAndIsm(
+            1380012617,
+            TypeCasts.addressToBytes32(address(0x68B1D87F95878fE05B998F19b66F4baba5De1aed)),
+            TypeCasts.addressToBytes32(address(0x9A676e781A523b5d0C0e43731313A708CB607508))
+        );
+
+        IInterchainGasPaymaster gasPaymaster = IInterchainGasPaymaster(0x457cCf29090fe5A24c19c1bc95F492168C0EaFdb);
+
+        uint256 gasPayment = gasPaymaster.quoteGasPayment(1380012617, 50_000);
+        uint256 gasPayment2 =
+            InterchainAccountRouter(address(0x8A93d247134d91e0de6f96547cB0204e5BE8e5D8)).quoteGasPayment(1380012617);
+
+        counter.icaTest{value: gasPayment + gasPayment2}();
     }
+}
+
+interface IInterchainAccountRouter {
+    function callRemote(uint32 _destinationDomain, CallLib.Call[] calldata calls) external payable returns (bytes32);
+
+    function getRemoteInterchainAccount(uint32 _destination, address _owner) external view returns (address);
 }
 
 // contract AnvilTest is Test {
