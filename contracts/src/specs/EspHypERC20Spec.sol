c8ef53bb25fbbad29af2dac7609e83150bfbdb9c
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2025-09-09 10:31:56 -0400

    Actually add specs

---
+++ contracts/src/specs/EspHypERC20Spec.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.30;
+
+import {HypERC20} from "@hyperlane-core/solidity/contracts/token/HypERC20.sol";
+
+contract EspHypERC20Spec is HypERC20 {
+    constructor(uint8 __decimals, uint256 _scale, address _mailbox) HypERC20(__decimals, _scale, _mailbox) {}
+}
