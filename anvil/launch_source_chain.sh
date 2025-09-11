d4a00f8f6bbc55b3c5a7dbb987bf215b0e502419
Mathis <sveitser@gmail.com>
2025-09-11 17:34:17 +0200

    Merge with main, add hyperlane-cli to local dev env (#13)
    * deps: install all dependencies with soldeer (#12)
    
    * deps: install all dependencies with soldeer
    
    I couldn't compile the project on a fresh checkout because the
    submodules didn't install.
    
    - Move all dependencies to soldeer
    - Add missing transitive dependencies
    - Add manual remappings to fix compilation
    - Remove broken submodule config
    - Add flake.nix
    - Separate CI jobs
    
    * CI: try with git url override
    
    * fix: forge soldeer update
    
    Previously failed with an unzipping error.
    
    * add hyperlane-cli to nix dev env
    
    * add nix-direnv
    
    * add .direnv to gitignore
    
    * update anvil nodes setup and redeploy
    
    ---------
    
    Co-authored-by: ytumanov <airhole.ua@gmail.com>

--- anvil/launch_source_chain.sh
+++ anvil/launch_source_chain.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 source .hyperlane_env
 # rm ./hyperlane/chains/source/state.json
-# anvil --port $RPC_SOURCE_CHAIN_PORT --chain-id $SOURCE_CHAIN_ID --dump-state ./hyperlane/chains/source/state.json --block-time 5
-anvil --port $RPC_SOURCE_CHAIN_PORT --chain-id $SOURCE_CHAIN_ID --load-state ./hyperlane/chains/source/state.json --block-time 5
+# anvil --port $RPC_SOURCE_CHAIN_PORT --chain-id $SOURCE_CHAIN_ID --dump-state ./hyperlane/chains/source/state.json --block-time 2 --mixed-mining
+anvil --port $RPC_SOURCE_CHAIN_PORT --chain-id $SOURCE_CHAIN_ID --load-state ./hyperlane/chains/source/state.json --block-time 2 --mixed-mining
---
+++ flake.nix
@@ -0,0 +1,88 @@
+{
+  description = "Espresso Moka bridge";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.foundry-nix.url = "github:shazow/foundry.nix/stable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.solc-bin.url = "github:EspressoSystems/nix-solc-bin";
+
+  outputs =
+    { nixpkgs
+    , foundry-nix
+    , flake-utils
+    , solc-bin
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      solhintPkg = { buildNpmPackage, fetchFromGitHub }:
+        buildNpmPackage rec {
+          pname = "solhint";
+          # To update the solhint version:
+          # 1. update the version tag
+          # 2  uncomment empty hash and npmDepsHash
+          # 3. activate `nix develop` and fill in the new hash
+          # 4. activate `nix develop` again, fill in the npmDepsHash.
+          version = "5.1.0";
+          src = fetchFromGitHub {
+            owner = "protofire";
+            repo = pname;
+            rev = "refs/tags/v${version}";
+            # hash = "";
+            hash = "sha256-hWf+4vSSqjSvN2SFC0z07QvnhQj1IWy3G1jh/E8Fuv8=";
+          };
+          # npmDepsHash = "";
+          npmDepsHash = "sha256-DbkjOZ/TtHHvmWPgQA8yuoTFLfXQg0LYwe9caZ0tCOc=";
+          dontNpmBuild = true;
+        };
+      hyperlanePkg = { stdenv, nodejs, fetchurl, makeWrapper }:
+        stdenv.mkDerivation rec {
+          pname = "hyperlane-cli";
+          version = "18.2.0";
+
+          src = fetchurl {
+            url = "https://registry.npmjs.org/@hyperlane-xyz/cli/-/cli-${version}.tgz";
+            hash = "sha256-Qhg0GJpVVLxIzhFE4bUhI2TOh7hP12ZF767c+dv8GPY=";
+          };
+
+          nativeBuildInputs = [ makeWrapper ];
+          buildInputs = [ nodejs ];
+
+          installPhase = ''
+            mkdir -p $out/lib/node_modules/@hyperlane-xyz/cli
+            cp -r . $out/lib/node_modules/@hyperlane-xyz/cli
+
+            mkdir -p $out/bin
+            makeWrapper ${nodejs}/bin/node $out/bin/hyperlane \
+              --add-flags "$out/lib/node_modules/@hyperlane-xyz/cli/cli-bundle/index.js"
+          '';
+        };
+      overlays = [
+        foundry-nix.overlay
+        solc-bin.overlays.default
+        (final: prev: {
+          solhint =
+            solhintPkg { inherit (prev) buildNpmPackage fetchFromGitHub; };
+          hyperlane-cli =
+            hyperlanePkg { inherit (prev) stdenv nodejs fetchurl makeWrapper; };
+        })
+      ];
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = overlays;
+      };
+    in
+    {
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            foundry-bin
+            solc
+            solhint
+            hyperlane-cli
+            pre-commit
+          ];
+        };
+    });
+
+}
