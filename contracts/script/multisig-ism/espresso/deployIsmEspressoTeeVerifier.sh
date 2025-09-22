d4bb825098c838e462e3c2b975eaa835f1928547
ytumanov <airhole.ua@gmail.com>
2025-09-22 16:54:01 +0200

    move esspresso ism tee verifier and rewrite scripts

---
+++ contracts/script/multisig-ism/espresso/deployIsmEspressoTeeVerifier.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Load .env
+export $(grep -v '^#' .env | xargs)
+
+echo "Deploying ISMEspressoTEEVerifier on the source chain"
+ESPRESSO_TEE_VERIFIER_ADDRESS=$DESTINATION_ESPRESSO_TEE_VERIFIER_ADDRESS
+forge script script/multisig-ism/espresso/ISMEspressoTEEVerifier.s.sol:ISMEspressoTEEVerifierScript  --rpc-url $SOURCE_CHAIN_RPC_URL --private-key $DEPLOYER_PRIVATE_KEY --broadcast --via-ir
+
+echo "Deploying ISMEspressoTEEVerifier on the destination chain"
+ESPRESSO_TEE_VERIFIER_ADDRESS=$DESTINATION_ESPRESSO_TEE_VERIFIER_ADDRESS
+forge script script/multisig-ism/espresso/ISMEspressoTEEVerifier.s.sol:ISMEspressoTEEVerifierScript  --rpc-url $DESTINATION_CHAIN_RPC_URL --private-key $DEPLOYER_PRIVATE_KEY --broadcast --via-ir
+
+echo "ISM espresso verifiers deployed"
