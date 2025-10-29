abba0df35b65d6167e3936234dd80f7674997028
ifelsedeveloper <xboxfadeev@gmail.com>
2025-10-29 13:01:06 +0000

    feat: add deploy proxy admin

--- contracts/script/token/DeployProxyAdmin.s.sol
+++ contracts/script/token/DeployProxyAdmin.s.sol
@@ -1,21 +1,22 @@
 // <ai_context>
 // This script deploys ProxyAdmin using CREATE3 for deterministic address.
 // </ai_context>
-
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.30;
-
 import "forge-std/src/Script.sol";
 import "forge-std/src/console.sol";
-import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {ProxyAdmin} from "@openzeppelin-contracts-v5/proxy/transparent/ProxyAdmin.sol";
 import {ICREATE3Factory} from "../../src/ICREATE3Factory.sol";
-
 contract DeployProxyAdminScript is Script {
     function run() public {
         address factoryAddr = vm.envAddress("CREATE3_FACTORY_ADDRESS");
         ICREATE3Factory factory = ICREATE3Factory(factoryAddr);
         bytes32 saltBytes = keccak256(bytes(vm.envString("SALT")));
-        bytes memory creationCode = type(ProxyAdmin).creationCode;
+        address owner = vm.envAddress("OWNER");
+        bytes memory creationCode = abi.encodePacked(
+            type(ProxyAdmin).creationCode,
+            abi.encode(owner)
+        );
         vm.startBroadcast();
         address deployed = factory.deploy(saltBytes, creationCode);
         vm.stopBroadcast();
