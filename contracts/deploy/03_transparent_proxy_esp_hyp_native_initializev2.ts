3933da6dd9347cf0dd81b382d1ee372917c735ce
ifelsedeveloper <xboxfadeev@gmail.com>
2025-11-06 14:44:55 +0000

    feat: add deploy

--- contracts/deploy/03_transparent_proxy_esp_hyp_native_initializev2.ts
+++ contracts/deploy/03_transparent_proxy_esp_hyp_native_initializev2.ts
@@ -4,7 +4,7 @@ import { deployWithCreate3 } from "../utils";
 import { EspHypNative__factory } from "../typechain-types";
 const SALT_STRING = "EspHypNativeTransparentProxy-salt-v1";
 
-// TODO: complete via proxy transparent 
+// TODO: complete via proxy transparent initializeV2 - call
 const deployEspHypNativeTransparentProxy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts, network, ethers, deployments } = hre;
   const { deployer } = await getNamedAccounts();
@@ -12,14 +12,14 @@ const deployEspHypNativeTransparentProxy: DeployFunction = async function (hre: 
   const artifactName = "EspHypNative";
   const proxyAdminAddress = (await deployments.get("ProxyAdmin")).address;
   const espHypNativeImplementationAddress = (await deployments.get("EspHypNative_Implementation")).address;
-
-  const hook = process.env.HOOK!;
-  const interchainSecurityModule = process.env.INTERCHAIN_SECURITY_MODULE!;
-  const owner = process.env.OWNER!;
-
+  const mailboxAddress = process.env.MAILBOX_ADDRESS;
+  const nftSalePrice = process.env.NFT_SALE_PRICE_WEI ? parseInt(process.env.NFT_SALE_PRICE_WEI) : 1;
+  const destinationDomainId = process.env.DESTINATION_DOMAIN_ID ? parseInt(process.env.DESTINATION_DOMAIN_ID) : 1;
+  if (!mailboxAddress) {
+    throw new Error("MAILBOX_ADDRESS is not set");
+  }
   const espHypNative = EspHypNative__factory.connect(espHypNativeImplementationAddress);
-
-  const initializeDataCallData = espHypNative.interface.encodeFunctionData("initialize", [hook, interchainSecurityModule, owner]);
+  const initializeV2DataCallData = espHypNative.interface.encodeFunctionData("initializeV2", [nftSalePrice, destinationDomainId]);
   console.log("deployer", deployer);
   console.log("balance", ethers.formatEther(await ethers.provider.getBalance(deployer)));
   console.log("network", network.name, network.config.chainId, network.config);
@@ -28,7 +28,7 @@ const deployEspHypNativeTransparentProxy: DeployFunction = async function (hre: 
     contractName,
     deployer,
     salt: SALT_STRING,
-    constructorArgs: [proxyAdminAddress, espHypNativeImplementationAddress, initializeDataCallData],
+    constructorArgs: [proxyAdminAddress, espHypNativeImplementationAddress, initializeV2DataCallData],
     confirmations: 5,
     overrides: { gasLimit: 5_000_000 },
   });
