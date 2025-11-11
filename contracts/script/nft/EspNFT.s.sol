384a736613b2b0ec525de8994d7ad139fe38c767
ytumanov <airhole.ua@gmail.com>
2025-11-11 17:36:32 +0100

    add general 1 way dep0loy script, update specs

--- contracts/script/nft/EspNFT.s.sol
+++ contracts/script/nft/EspNFT.s.sol
@@ -13,8 +13,8 @@ contract EspNFTScript is Script {
     address payable espressoTreasury = payable(vm.envAddress("ESPRESSO_TREASURY_ADDRESS"));
     address payable partnerTreasury = payable(vm.envAddress("PARTNER_TREASURY_ADDRESS"));
     uint256 espressoTreasuryPercentage = vm.envUint("ESPRESSO_TREASURY_PERCENTAGE");
-    string name = "Espresso Composables NFT";
-    string symbol = "EC";
+    string name = vm.envString("NFT_NAME");
+    string symbol = vm.envString("NFT_SYMBOL");
     uint256 saleTimeStart = vm.envOr("SALE_TIME_START", block.timestamp + 10);
 
     function run() public {
