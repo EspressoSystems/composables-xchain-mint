10bffafae6600a1214d93659a9e7e816d7bf5238
ifelsedeveloper <xboxfadeev@gmail.com>
2025-11-20 10:35:25 +0000

    feat: add config

---
+++ contracts/utils/config.ts
@@ -0,0 +1,4 @@
+//HypeERC20 deploy & Hype Native (Ape chain and Rari chain settings the same)
+export const MAILBOX_ADDRESS = "0x017be100600eCee055Eb27FA3b318E05Db79caD6";
+export const DECIMALS = 18;
+export const SCALE = 1;
